5c084a664fed935d98db2da06507c53f099abecb
ringhtprince <ringhtprince18@gmail.com>
2017-04-26 18:51:00 -0500

    Update tcptweaker.sh

--- tcptweaker.sh
+++ tcptweaker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tput setaf 7 ; tput setab 4 ; tput bold ; printf '%35s%s%-20s\n' "TCP Tweaker 1.0" ; tput sgr0
+tput setaf 7 ; tput setab 4 ; tput bold ; printf '%35s%s%-20s\n' "TCP Tweaker 1.0 By RinghtPrince & MagodOz" ; tput sgr0
 if [[ `grep -c "^#PH56" /etc/sysctl.conf` -eq 1 ]]
 then
 	echo ""
